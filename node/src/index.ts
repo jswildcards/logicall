c8eab9bf5587adfaea532f3525f8f18c2d4e100b
jswildcards <js.wildcards@gmail.com>
2020-09-21 16:12:22 +0800

    minor change

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,13 +1,6 @@
 import express from 'express';
-import mysql from 'mysql';
+import connection from './modules/db';
 const app = express();
-
-const connection = mysql.createConnection({
-  host: 'mysql',
-  user: 'docker',
-  password: 'docker',
-  database: 'classicmodels'
-});
 
 app.get('/api', function(req, res) {
   res.json({
@@ -24,68 +17,52 @@ app.get('/api', function(req, res) {
   });
 });
 
-app.get('/api/customers', function(req, res) {
+app.get('/api/customers', async function(req, res) {
   const sql = 'SELECT * FROM customers';
-  connection.query(sql, function(err, result, fields) {
-    if (err) return res.json({err});
-    res.json({result});
-  });
+  const result = await connection.execute(sql);
+  res.json({ result });
 });
 
-app.get('/api/employees', function(req, res) {
+app.get('/api/employees', async function(req, res) {
   const sql = 'SELECT * FROM employees';
-  connection.query(sql, function(err, result, fields) {
-    if (err) return res.json({err});
-    res.json({result});
-  });
+  const result = await connection.execute(sql);
+  res.json({ result });
 });
 
-app.get('/api/offices', function(req, res) {
+app.get('/api/offices', async function(req, res) {
   const sql = 'SELECT * FROM offices';
-  connection.query(sql, function(err, result, fields) {
-    if (err) return res.json({err});
-    res.json({result});
-  });
+  const result = await connection.execute(sql);
+  res.json({ result });
 });
 
-app.get('/api/orderdetails', function(req, res) {
+app.get('/api/orderdetails', async function(req, res) {
   const sql = 'SELECT * FROM orderdetails';
-  connection.query(sql, function(err, result, fields) {
-    if (err) return res.json({err});
-    res.json({result});
-  });
+  const result = await connection.execute(sql);
+  res.json({ result });
 });
 
-app.get('/api/orders', function(req, res) {
+app.get('/api/orders', async function(req, res) {
   const sql = 'SELECT * FROM orders';
-  connection.query(sql, function(err, result, fields) {
-    if (err) return res.json({err});
-    res.json({result});
-  });
+  const result = await connection.execute(sql);
+  res.json({ result });
 });
 
-app.get('/api/payments', function(req, res) {
+app.get('/api/payments', async function(req, res) {
   const sql = 'SELECT * FROM payments';
-  connection.query(sql, function(err, result, fields) {
-    if (err) return res.json({err});
-    res.json({result});
-  });
+  const result = await connection.execute(sql);
+  res.json({ result });
 });
 
-app.get('/api/productlines', function(req, res) {
+app.get('/api/productlines', async function(req, res) {
   const sql = 'SELECT * FROM productlines';
-  connection.query(sql, function(err, result, fields) {
-    if (err) return res.json({err});
-    res.json({result});
-  });
+  const result = await connection.execute(sql);
+  res.json({ result });
 });
 
-app.get('/api/products', function(req, res) {
+app.get('/api/products', async function(req, res) {
   const sql = 'SELECT * FROM products';
-  connection.query(sql, function(err, result, fields) {
-    if (err) return res.json({err});
-    res.json({result});
-  });
+  const result = await connection.execute(sql);
+  res.json({ result });
 });
 
 app.listen(3000);
