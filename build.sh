da7a786623429d9a86be5efd15f2e1708a84baf6
jswildcards <js.wildcards@gmail.com>
2020-09-22 16:14:03 +0800

    add adminer

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker kill node mysql nginx
-docker rm node mysql nginx
+docker kill node mysql nginx adminer
+docker rm node mysql nginx adminer
 docker rmi -f logicall_node:latest logicall_nginx:latest logicall_mysql:latest
 docker-compose -p logicall up -d
