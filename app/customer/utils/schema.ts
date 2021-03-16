299aaf70827aa920ce3d3fbe67c310713a591cdb
jswildcards <js.wildcards@gmail.com>
2021-03-16 09:28:13 +0800

    fix driver phone missing when customer check order detail

--- app/customer/utils/schema.ts
+++ app/customer/utils/schema.ts
@@ -63,6 +63,7 @@ export const schema = {
                 username
                 firstName
                 lastName
+                phone
               }
             }
             status
@@ -108,6 +109,7 @@ export const schema = {
                 username
                 firstName
                 lastName
+                phone
               }
             }
             receiveAddress
